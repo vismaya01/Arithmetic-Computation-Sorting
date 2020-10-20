2d2a9dfe64964b16a398c723da9aa01bafed3410
vismaya01 <kvvismaya98@gmail.com>
2020-10-20 19:56:22 +0530

    Add : To take the input

--- arithmeticComputationAndSorting.sh
+++ arithmeticComputationAndSorting.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 echo "Arithmetic Compution And Sorting"
+#TO TAKING INPUT FROM USER
+read -p "Enter first input :" firstInput
+read -p "Enter second input:" secondInput
+read -p "Enter third input :" thirdInput
 
